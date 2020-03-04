0f5d7c638391425defad1b043102cde4ca2377be
Anton TCVETKOV <anton_tcvetkov@epam.com>
2020-03-04 12:52:37 +0300

    my fixes

--- scripts/kr8sswordz-pages.sh
+++ scripts/kr8sswordz-pages.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #Retrieve the latest git commit hash
-BUILD_TAG=`git rev-parse --short HEAD`
+BUILD_TAG=$(git rev-parse --short HEAD)
 
 #Build the docker image
-docker build -t 127.0.0.1:30400/kr8sswordz:$BUILD_TAG -f applications/kr8sswordz-pages/Dockerfile applications/kr8sswordz-pages
+docker build -t 127.0.0.1:30400/kr8sswordz:"$BUILD_TAG" -f applications/kr8sswordz-pages/Dockerfile applications/kr8sswordz-pages
 
 #Setup the proxy for the registry
-docker stop socat-registry; docker rm socat-registry; docker run -d -e "REG_IP=`minikube ip`" -e "REG_PORT=30400" --name socat-registry -p 30400:5000 socat-registry
+docker stop socat-registry; docker rm socat-registry; docker run -d -e "REG_IP=$(minikube ip)" -e "REG_PORT=30400" --name socat-registry -p 30400:5000 socat-registry
 
 echo "5 second sleep to make sure the registry is ready"
 sleep 5;
 
 #Push the images
-docker push 127.0.0.1:30400/kr8sswordz:$BUILD_TAG
+docker push 127.0.0.1:30400/kr8sswordz:"$BUILD_TAG"
 
 #Stop the registry proxy
 docker stop socat-registry
